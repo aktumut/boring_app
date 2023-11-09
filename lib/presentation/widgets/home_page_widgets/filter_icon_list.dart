49b44fd4a63011686f8f285d9d4947cc82e3b58a
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-11-09 20:01:59 +0100

    create filter_icon_list.dart

---
+++ lib/presentation/widgets/home_page_widgets/filter_icon_list.dart
@@ -0,0 +1,45 @@
+import 'package:boring_app/business_logic/blocs/activity/activity_cubit.dart';
+import 'package:boring_app/presentation/widgets/home_page_widgets/filter_icon.dart';
+import 'package:boring_app/utils/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class FilterIconList extends StatelessWidget {
+  const FilterIconList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ActivityCubit, ActivityState>(
+      builder: (BuildContext context, ActivityState state) {
+        if (state is ActivityLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is ActivityLoaded) {
+          return _buildFilterList(
+              state.activityTypes, state.selectedFilter, context);
+        } else if (state is ActivityError) {
+          return Center(child: Text(state.message));
+        } else {
+          return const Center(child: Text('Unknown State'));
+        }
+      },
+    );
+  }
+
+  Widget _buildFilterList(
+      List<String> activityTypes, String selectedFilter, BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: tSpaceBase),
+      height: tHeightFilterWidget,
+      width: double.infinity,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: activityTypes.length,
+        itemBuilder: (BuildContext context, int index) {
+          final String activityType = activityTypes[index];
+          final bool isSelected = selectedFilter == activityType;
+          return FilterIcon(activityType: activityType, isSelected: isSelected);
+        },
+      ),
+    );
+  }
+}
