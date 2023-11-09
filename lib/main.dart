48802f396eb1a02055278e740b988a95ee226a92
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-11-09 09:46:49 +0100

    create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:boring_app/app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+  runApp(BoringApp());
+}
