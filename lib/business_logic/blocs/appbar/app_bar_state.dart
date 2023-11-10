84b87362500cc9828815d7a78ef74a776113c40f
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-11-10 12:16:01 +0100

    add appbarwithfilters state

--- lib/business_logic/blocs/appbar/app_bar_state.dart
+++ lib/business_logic/blocs/appbar/app_bar_state.dart
@@ -1,15 +1,28 @@
 part of 'app_bar_cubit.dart';
 
 abstract class AppBarState extends Equatable {
-  const AppBarState();
+  AppBarState();
 }
 
 class AppBarExpanded extends AppBarState {
+  AppBarExpanded() : super();
+
   @override
   List<Object> get props => [];
 }
 
 class AppBarCollapsed extends AppBarState {
+  AppBarCollapsed() : super();
+
   @override
   List<Object> get props => [];
+}
+
+class AppBarWithAllFilters extends AppBarState {
+  double heightAppBarWithFilters;
+
+  AppBarWithAllFilters(this.heightAppBarWithFilters) : super();
+
+  @override
+  List<Object> get props => [heightAppBarWithFilters];
 }
