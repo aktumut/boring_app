79a4f3ab26a12c24687c56ea11b04b487b0343ea
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-11-10 12:24:19 +0100

    create constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,12 @@
+// Texts
+const String tTextActivity = 'Local Activities';
+const String tTextSlogan = 'What To Do?';
+const String tTextAppName = 'Boring App';
+const String tTextCallToAction = 'Show all filters';
+const String tTextBoldFont = 'Roboto-Bold';
+const String tTextRegularFont = 'Roboto-Regular';
+
+// Api
+const String tTextBaseUrl = 'http://www.boredapi.com/api';
+const String tTextFilterAll = 'All';
+const double tNumberOfActivities = 10;
