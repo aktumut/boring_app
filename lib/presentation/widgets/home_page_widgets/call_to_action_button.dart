75b6aa4b3ca102edec89987775eae31fe1e3212a
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-11-10 12:23:08 +0100

    create CallToActionButton

---
+++ lib/presentation/widgets/home_page_widgets/call_to_action_button.dart
@@ -0,0 +1,43 @@
+import 'package:boring_app/business_logic/blocs/activity/activity_cubit.dart';
+import 'package:boring_app/business_logic/blocs/appbar/app_bar_cubit.dart';
+import 'package:boring_app/helpers/determine_app_bar_height.dart';
+import 'package:boring_app/utils/constants.dart';
+import 'package:boring_app/utils/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CallToActionButton extends StatelessWidget {
+  const CallToActionButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: tSpace,
+      left: tSpaceDoubleHorizontalPadding,
+      right: tSpaceDoubleHorizontalPadding,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: tColorWhite,
+          foregroundColor: tColorPrimary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(tRadiusButton),
+          ),
+        ),
+        onPressed: () {
+          _handleButtonPress(context);
+        },
+        child: const Text(
+          tTextCallToAction,
+          style: tTextStyleButton,
+        ),
+      ),
+    );
+  }
+
+  void _handleButtonPress(BuildContext context) {
+    final appBarHeight = determineAppBarHeight(
+      context.read<ActivityCubit>().activityTypes.length,
+    );
+    context.read<AppBarCubit>().expandAppBarWithAllFilters(appBarHeight);
+  }
+}
