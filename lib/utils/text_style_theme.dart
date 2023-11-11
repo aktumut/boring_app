36d3fd3c088f0d94b473391d8080a5b125822670
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-11-11 23:39:02 +0100

    create text_style_theme

---
+++ lib/utils/text_style_theme.dart
@@ -0,0 +1,61 @@
+
+
+import 'package:boring_app/utils/constants.dart';
+import 'package:boring_app/utils/theme.dart';
+import 'package:flutter/cupertino.dart';
+
+const TextStyle tTextStyleHeader1 = TextStyle(
+  fontSize: tFontSizeLarge,
+  color: tColorWhite,
+  fontFamily: tTextBoldFont,
+  height: 1.5,
+);
+
+
+const TextStyle tTextStyleHeader2 = TextStyle(
+  fontSize: tFontSizeMedium,
+  color: tColorWhite,
+  fontFamily: tTextBoldFont,
+  height: 1,
+);
+
+const TextStyle tTextStyleHeader3 = TextStyle(
+  fontSize: tFontSizeSmall,
+  color: tColorGray,
+  fontFamily: tTextBoldFont,
+  height: 1,
+);
+
+const TextStyle tTextStyleButton = TextStyle(
+  fontSize: tFontSizeButton,
+  fontFamily: tTextBoldFont,
+  height: 1,
+);
+
+const TextStyle tTextStyleFilterIcon = TextStyle(
+  fontSize: tFontSizeFilterIcon,
+  color: tColorWhite,
+  fontFamily: tTextBoldFont,
+  height: 1,
+);
+
+const TextStyle tTextStyleActivityListTileHeader = TextStyle(
+  fontSize: tFontSizeSmall,
+  color: tColorPrimary,
+  fontFamily: tTextBoldFont,
+  height: 1,
+);
+
+const TextStyle tTextStyleActivityListTileSubHeader = TextStyle(
+  fontSize: tFontSizeTiny,
+  color: tColorDarkGray,
+  fontFamily: tTextRegularFont,
+  height: 1,
+);
+
+const TextStyle tTextStyleActivityListTileActivity = TextStyle(
+  fontSize: tFontSizeSmall,
+  color: tColorBlack,
+  fontFamily: tTextBoldFont,
+  height: 1,
+);
