f00ecf216fd2db6f2cfcfd4ac18cdbf2dc4b2c79
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-11-11 19:01:07 +0100

    create home_page

---
+++ lib/presentation/pages/home/home_page.dart
@@ -0,0 +1,70 @@
+import 'package:boring_app/business_logic/blocs/appbar/app_bar_cubit.dart';
+import 'package:boring_app/presentation/widgets/home_page_widgets/activity_list.dart';
+import 'package:boring_app/presentation/widgets/home_page_widgets/call_to_action_button.dart';
+import 'package:boring_app/presentation/widgets/home_page_widgets/curved_shape_painter.dart';
+import 'package:boring_app/presentation/widgets/home_page_widgets/custom_sliver_appbar.dart';
+import 'package:boring_app/utils/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomePage extends StatelessWidget {
+  ScrollController scrollController;
+
+  HomePage({required this.scrollController, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AppBarCubit, AppBarState>(
+      builder: (BuildContext context, AppBarState state) {
+        return Scaffold(
+            body: CustomScrollView(
+              physics: state is! AppBarWithAllFilters
+                  ? const ClampingScrollPhysics()
+                  : const NeverScrollableScrollPhysics(),
+              controller: scrollController,
+              slivers: <Widget>[
+                CustomSliverAppBar(
+                  scrollController: scrollController,
+                ),
+                SliverToBoxAdapter(
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    children: <Widget>[
+                      Positioned(
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        height: tShapeHeight,
+                        child: CustomPaint(
+                          painter: CurvedShapePainter(),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: tSpaceHorizontalPadding,
+                          vertical: tSpaceDoubleVerticalPadding,
+                        ),
+                        child: ActivityList(),
+                      ),
+                      state is! AppBarWithAllFilters
+                          ? const CallToActionButton()
+                          : Container(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            floatingActionButton: state is AppBarWithAllFilters
+                ? FloatingActionButton(
+                    child: const Icon(
+                      Icons.close,
+                      color: tColorWhite,
+                    ),
+                    onPressed: () {
+                      context.read<AppBarCubit>().resetAppBar();
+                    })
+                : null);
+      },
+    );
+  }
+}
