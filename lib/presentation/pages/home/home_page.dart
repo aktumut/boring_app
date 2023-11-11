16e1e335dfcaeae8eec3fa3ec8b1de275d226787
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-11-11 19:34:54 +0100

    add ResetAppBarButton, cancel floatingActionButton

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -3,6 +3,7 @@ import 'package:boring_app/presentation/widgets/home_page_widgets/activity_list.
 import 'package:boring_app/presentation/widgets/home_page_widgets/call_to_action_button.dart';
 import 'package:boring_app/presentation/widgets/home_page_widgets/curved_shape_painter.dart';
 import 'package:boring_app/presentation/widgets/home_page_widgets/custom_sliver_appbar.dart';
+import 'package:boring_app/presentation/widgets/home_page_widgets/reset_app_bar_button.dart';
 import 'package:boring_app/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,53 +18,47 @@ class HomePage extends StatelessWidget {
     return BlocBuilder<AppBarCubit, AppBarState>(
       builder: (BuildContext context, AppBarState state) {
         return Scaffold(
-            body: CustomScrollView(
-              physics: state is! AppBarWithAllFilters
-                  ? const ClampingScrollPhysics()
-                  : const NeverScrollableScrollPhysics(),
-              controller: scrollController,
-              slivers: <Widget>[
-                CustomSliverAppBar(
-                  scrollController: scrollController,
-                ),
-                SliverToBoxAdapter(
-                  child: Stack(
-                    clipBehavior: Clip.none,
-                    children: <Widget>[
-                      Positioned(
-                        top: 0,
-                        left: 0,
-                        right: 0,
-                        height: tShapeHeight,
-                        child: CustomPaint(
-                          painter: CurvedShapePainter(),
-                        ),
+          body: CustomScrollView(
+            physics: state is! AppBarWithAllFilters
+                ? const ClampingScrollPhysics()
+                : const NeverScrollableScrollPhysics(),
+            controller: scrollController,
+            slivers: <Widget>[
+              CustomSliverAppBar(
+                scrollController: scrollController,
+              ),
+              SliverToBoxAdapter(
+                child: Stack(
+                  clipBehavior: Clip.none,
+                  children: <Widget>[
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      height: tShapeHeight,
+                      child: CustomPaint(
+                        painter: CurvedShapePainter(),
                       ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: tSpaceHorizontalPadding,
-                          vertical: tSpaceDoubleVerticalPadding,
-                        ),
-                        child: ActivityList(),
-                      ),
-                      state is! AppBarWithAllFilters
-                          ? const CallToActionButton()
-                          : Container(),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            floatingActionButton: state is AppBarWithAllFilters
-                ? FloatingActionButton(
-                    child: const Icon(
-                      Icons.close,
-                      color: tColorWhite,
                     ),
-                    onPressed: () {
-                      context.read<AppBarCubit>().resetAppBar();
-                    })
-                : null);
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: tSpaceHorizontalPadding,
+                        vertical: tSpaceDoubleVerticalPadding,
+                      ),
+                      child: ActivityList(),
+                    ),
+                    state is! AppBarWithAllFilters
+                        ? const CallToActionButton()
+                        : Container(),
+                    state is AppBarWithAllFilters
+                        ? const ResetAppBarButton()
+                        : Container(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
       },
     );
   }
