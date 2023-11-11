7cf281f459bdb91f543ca87e3ca70e9c30bce77a
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-11-11 19:06:27 +0100

    fix logic

--- lib/helpers/determine_app_bar_height.dart
+++ lib/helpers/determine_app_bar_height.dart
@@ -1,9 +1,9 @@
 import 'package:boring_app/utils/theme.dart';
 
 double determineAppBarHeight(int activityTypesLength) {
-  if (5 < activityTypesLength && activityTypesLength <= 9) {
+  if (6 < activityTypesLength && activityTypesLength <= 9) {
     return tHeightFinalAppBar6to8;
-  } else if (2 < activityTypesLength && activityTypesLength <= 5) {
+  } else if (3 < activityTypesLength && activityTypesLength <= 6) {
     return tHeightFinalAppBar3to5;
   } else if (0 < activityTypesLength && activityTypesLength <= 3) {
     return tHeightFinalAppBar1to2;
