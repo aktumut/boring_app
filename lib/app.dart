6bf67f81f887a434001120ad8cdb0fa49e3d8db6
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-11-11 19:02:26 +0100

    add Providers, dependency injection

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,5 @@
+import 'package:boring_app/business_logic/blocs/activity/activity_cubit.dart';
+import 'package:boring_app/business_logic/blocs/activity_like/activity_like_cubit.dart';
 import 'package:boring_app/business_logic/blocs/appbar/app_bar_cubit.dart';
 import 'package:boring_app/utils/constants.dart';
 import 'package:boring_app/utils/theme.dart';
@@ -7,18 +9,37 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'presentation/pages/home/home_page.dart';
 
 class BoringApp extends StatelessWidget {
-  const BoringApp({super.key});
+  final ActivityCubit activityCubit;
+  final ScrollController scrollController;
+
+  const BoringApp({
+    super.key,
+    required this.activityCubit,
+    required this.scrollController,
+  });
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (BuildContext context) => AppBarCubit()),
+        BlocProvider(
+          create: (BuildContext context) => AppBarCubit(scrollController),
+        ),
+        BlocProvider.value(
+          value: activityCubit..fetchActivities(),
+        ),
+        BlocProvider(
+          create: (BuildContext context) =>
+              ActivityLikeCubit(activityCubit)..init(),
+        ),
       ],
       child: MaterialApp(
         title: tTextAppName,
         theme: appThemeData,
-        home: HomePage(),
+        debugShowCheckedModeBanner: false,
+        home: HomePage(
+          scrollController: scrollController,
+        ),
       ),
     );
   }
