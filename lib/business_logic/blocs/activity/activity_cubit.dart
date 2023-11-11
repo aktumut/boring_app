96c5fc513e7da16b1cdad1fcd41a1b9412f9060b
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-11-11 18:59:02 +0100

    add shared_preferences, updateActivityLikes

--- lib/business_logic/blocs/activity/activity_cubit.dart
+++ lib/business_logic/blocs/activity/activity_cubit.dart
@@ -1,45 +1,58 @@
+import 'dart:math' as math;
+
+import 'package:boring_app/business_logic/blocs/activity_like/activity_like_cubit.dart';
 import 'package:boring_app/business_logic/repositories/activity_repository.dart';
 import 'package:boring_app/data/models/activity_model.dart';
 import 'package:boring_app/utils/constants.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'activity_state.dart';
 
 class ActivityCubit extends Cubit<ActivityState> {
   final ActivityRepository _activityRepository;
-  List<ActivityModel> _allActivities = [];
+  final List<ActivityModel> _allActivities = [];
   List<String> activityTypes = [];
 
   ActivityCubit(this._activityRepository) : super(ActivityLoading());
 
   Future<void> fetchActivities() async {
     try {
-      _allActivities = [];
-      Set<String> activityTypesSet = {tTextAll};
+      Set<String> activityTypesSet = {tTextFilterAll};
+      _allActivities.clear();
 
       for (int i = 0; i < tNumberOfActivities; i++) {
-        final ActivityModel activity =
-            await _activityRepository.fetchActivity();
+        ActivityModel activity = await _activityRepository.fetchActivity();
+        activity.likes = math.Random().nextInt(tMaxLikes);
         _allActivities.add(activity);
         activityTypesSet.add(activity.type!);
       }
 
+      await _updateActivityLikes();
       activityTypes = activityTypesSet.toList();
-      emit(ActivityLoaded(_allActivities, activityTypes, tTextAll));
-    } catch (e) {
-      emit(ActivityError(e.toString()));
+
+      emit(ActivityLoaded(_allActivities, activityTypes, tTextFilterAll));
+    } catch (exception) {
+      emit(ActivityError(exception.toString()));
+    }
+  }
+
+  Future<void> _updateActivityLikes() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    for (var activity in _allActivities) {
+      activity.isLiked = prefs.getBool(activity.key!) ?? false;
     }
   }
 
   void filterActivities(String selectedType) {
-    List<ActivityModel> filteredActivities = selectedType == tTextAll
+    List<ActivityModel> filteredActivities = selectedType == tTextFilterAll
         ? _allActivities
         : _allActivities
-            .where((ActivityModel activity) => activity.type == selectedType)
+            .where((activity) => activity.type == selectedType)
             .toList();
 
-    if (state is ActivityLoaded) {
+    if (state is ActivityLoaded || state is ActivityLikeUpdate) {
       ActivityLoaded currentState = state as ActivityLoaded;
       emit(ActivityLoaded(
           filteredActivities, currentState.activityTypes, selectedType));
