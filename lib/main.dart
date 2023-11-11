e2936e96cbcef0f5df6a2d42bd14f027f63e9bb2
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-11-11 19:05:20 +0100

    add dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,32 @@
 import 'package:boring_app/app.dart';
+import 'package:boring_app/business_logic/blocs/activity/activity_cubit.dart';
+import 'package:boring_app/business_logic/repositories/activity_repository.dart';
+import 'package:boring_app/data/providers/network_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
 
-  runApp(BoringApp());
+  final Widget app = initializeApp();
+
+  runApp(app);
+}
+
+Widget initializeApp() {
+  final NetworkProvider networkProvider = NetworkProvider();
+  final ActivityRepository activityRepository =
+      ActivityRepository(networkProvider);
+  final ScrollController scrollController = ScrollController();
+  final ActivityCubit activityCubit = ActivityCubit(activityRepository)
+    ..fetchActivities();
+
+  return BoringApp(
+    activityCubit: activityCubit,
+    scrollController: scrollController,
+  );
 }
