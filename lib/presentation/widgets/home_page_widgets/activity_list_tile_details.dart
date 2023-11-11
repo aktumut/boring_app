b8ce53a11bbded1aebb91f1002487c8082d0a258
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-11-11 17:57:16 +0100

    create activity_list_tile_details

---
+++ lib/presentation/widgets/home_page_widgets/activity_list_tile_details.dart
@@ -0,0 +1,80 @@
+import 'package:boring_app/data/models/activity_model.dart';
+import 'package:boring_app/helpers/capitalize_first_letter.dart';
+import 'package:boring_app/helpers/color_based_on_price.dart';
+import 'package:boring_app/helpers/format_accessibility.dart';
+import 'package:boring_app/helpers/format_activity_text.dart';
+import 'package:boring_app/helpers/format_price.dart';
+import 'package:boring_app/utils/theme.dart';
+import 'package:flutter/material.dart';
+
+class ActivityListTileDetails extends StatelessWidget {
+  final ActivityModel activity;
+
+  const ActivityListTileDetails({super.key, required this.activity});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          const EdgeInsets.symmetric(horizontal: tSpaceHalfHorizontalPadding),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _buildActivityDescription(),
+          _buildActivityPricing(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildActivityDescription() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: tSpaceBase),
+        Text(capitalizeFirstLetter(activity.type ?? 'Random activity'),
+            style: tTextStyleActivityListTileHeader),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: tSpaceQuarter),
+          child: _buildAccessibilityAndParticipants(),
+        ),
+        const SizedBox(height: tSpaceHalf),
+        Text(
+          formatActivityText(activity.activity ?? 'Random activity'),
+          style: tTextStyleActivityListTileActivity,
+          overflow: TextOverflow.ellipsis,
+          maxLines: 1,
+        ),
+        const SizedBox(height: tSpaceDouble),
+      ],
+    );
+  }
+
+  Widget _buildAccessibilityAndParticipants() {
+    return Row(
+      children: [
+        const Icon(Icons.person,
+            size: tSizeActivityListTileIcon, color: tColorDarkGray),
+        Text(
+          '${activity.participants.toString()} | accessiblity: ${formatAccessibility(activity.accessibility!)}/10',
+          style: tTextStyleActivityListTileSubHeader,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildActivityPricing() {
+    return Column(
+      children: [
+        Icon(Icons.euro,
+            size: tSizeActivityListTilePriceIcon,
+            color: getColorBasedOnPrice(activity.price!)),
+        const SizedBox(height: tSpaceHalf),
+        Text(
+          activity.price == 0 ? 'Free!' : '${formatPrice(activity.price!)}/10',
+          style: tTextStyleActivityListTileHeader,
+        ),
+      ],
+    );
+  }
+}
