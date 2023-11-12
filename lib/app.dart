abcc768075bccf8576255a0e86529c4cb2f16aee
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-11-12 20:35:50 +0100

    change package importing path

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,11 @@
 import 'package:boring_app/business_logic/blocs/activity/activity_cubit.dart';
 import 'package:boring_app/business_logic/blocs/activity_like/activity_like_cubit.dart';
 import 'package:boring_app/business_logic/blocs/appbar/app_bar_cubit.dart';
+import 'package:boring_app/presentation/pages/home_page.dart';
 import 'package:boring_app/utils/constants.dart';
 import 'package:boring_app/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'presentation/pages/home_page.dart';
 
 class BoringApp extends StatelessWidget {
   final ActivityCubit activityCubit;
