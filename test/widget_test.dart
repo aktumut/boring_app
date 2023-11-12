f4e43f8bc52c9041c05df94814f21ca6c67e2219
Umut Aktaş <74653231+aktumut@users.noreply.github.com>
2023-11-12 23:45:27 +0100

    create test CallToActionButton

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,51 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:boring_app/business_logic/blocs/appbar/app_bar_cubit.dart';
+import 'package:boring_app/presentation/widgets/home_page_widgets/call_to_action_button.dart';
+import 'package:boring_app/utils/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
-import 'package:boring_app/main.dart';
+import 'widget_test.mocks.dart';
 
+@GenerateMocks([AppBarCubit])
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const BoringApp());
+  group('CallToActionButton Tests', () {
+    late MockAppBarCubit mockAppBarCubit;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    setUp(() {
+      mockAppBarCubit = MockAppBarCubit();
+      when(mockAppBarCubit.state)
+          .thenReturn(const AppBarExpanded()); // Provide a valid AppBarState
+    });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    testWidgets(
+        'should render CallToActionButton and handle button press correctly',
+        (WidgetTester tester) async {
+      // Arrange
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      when(mockAppBarCubit.state).thenReturn(const AppBarExpanded());
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: MultiBlocProvider(
+            providers: [
+              BlocProvider<AppBarCubit>(create: (context) => mockAppBarCubit),
+            ],
+            child: const CallToActionButton(),
+          ),
+        ),
+      );
+
+      // Act
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+
+      // Assert
+      expect(find.byType(CallToActionButton), findsOneWidget);
+      verify(mockAppBarCubit.expandAppBarWithAllFilters(tHeightFinalAppBar1to2))
+          .called(1);
+    });
   });
 }
