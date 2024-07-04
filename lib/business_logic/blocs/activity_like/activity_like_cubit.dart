c628c467da8ba1beac3161f95e239b7c159b3814
Umut Aktaş <aktasumut@gmail.com>
2024-07-04 20:59:41 +0200

    del comments, add freezed config

--- lib/business_logic/blocs/activity_like/activity_like_cubit.dart
+++ lib/business_logic/blocs/activity_like/activity_like_cubit.dart
@@ -8,28 +8,21 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'activity_like_state.dart';
 
-/// Manages the like state of activities.
 class ActivityLikeCubit extends Cubit<ActivityLikeState> {
-  /// Initializes the [ActivityLikeCubit] with the [ActivityCubit].
   ActivityLikeCubit(this.activityCubit) : super(ActivityLikeInitial());
 
-  /// Reference to the [ActivityCubit] to react to its state changes.
   final ActivityCubit activityCubit;
 
-  /// Subscription to the [ActivityCubit]'s state stream.
   late final StreamSubscription<ActivityState> activitySubscription;
 
-  /// Local cache of all activities to manage their like states.
   List<ActivityModel> _allActivities = [];
 
-  /// Initializes the cubit by subscribing to the [activityCubit] stream.
   void init() {
     final currentState = activityCubit.state;
     if (currentState is ActivityLoaded) {
       _allActivities = currentState.activities;
     }
 
-    // Listen to activity state changes.
     activitySubscription = activityCubit.stream.listen(
       (ActivityState state) {
         if (state is ActivityLoaded) {
@@ -39,21 +32,23 @@ class ActivityLikeCubit extends Cubit<ActivityLikeState> {
     );
   }
 
-  /// Toggles the like state of an [activity] and updates the UI and storage.
   void toggleLike(ActivityModel activity) {
     final index =
         _allActivities.indexWhere((ActivityModel a) => a.key == activity.key);
     if (index != -1) {
-      _allActivities[index].isLiked = !_allActivities[index].isLiked;
-      _allActivities[index].likes += _allActivities[index].isLiked ? 1 : -1;
+      final updatedActivity = _allActivities[index].copyWith(
+        isLiked: !_allActivities[index].isLiked,
+        likes: _allActivities[index].isLiked
+            ? _allActivities[index].likes - 1
+            : _allActivities[index].likes + 1,
+      );
+      _allActivities[index] = updatedActivity;
 
-      // Update the shared preferences with the new like state.
       updateSharedPreferences(
         _allActivities[index].key!,
-        _allActivities[index].isLiked,
+        isLiked: _allActivities[index].isLiked,
       );
 
-      // Emit an update to the state with the new like count.
       emit(
         ActivityLikeUpdate(
           isLiked: _allActivities[index].isLiked,
@@ -63,7 +58,6 @@ class ActivityLikeCubit extends Cubit<ActivityLikeState> {
     }
   }
 
-  /// Cancels the subscription when the cubit is closed.
   @override
   Future<void> close() {
     activitySubscription.cancel();
