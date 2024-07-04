ee9c8dba59f0d6b35f178c2b756bac69ec165728
Umut Aktaş <aktasumut@gmail.com>
2024-07-04 21:02:36 +0200

    update

--- lib/business_logic/blocs/appbar/app_bar_cubit.dart
+++ lib/business_logic/blocs/appbar/app_bar_cubit.dart
@@ -7,12 +7,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'app_bar_state.dart';
 
-/// Manages the state of AppBar based on scroll events and provides
-/// animation for AppBar expansion.
 class AppBarCubit extends Cubit<AppBarState> {
-  /// Creates an instance of [AppBarCubit] with an attached [ScrollController].
-  ///
-  /// Initially, the state is set to [AppBarExpanded].
   AppBarCubit(this._scrollController) : super(const AppBarExpanded()) {
     _scrollController.addListener(_onScroll);
   }
@@ -28,9 +23,6 @@ class AppBarCubit extends Cubit<AppBarState> {
     emit(isAppBarExpanded ? const AppBarExpanded() : const AppBarCollapsed());
   }
 
-  /// Initiates an animation to expand the app bar height with a filter section.
-  ///
-  /// [finalAppBarHeight] is the height the app bar animates to.
   void expandAppBarWithAllFilters(double finalAppBarHeight) {
     final stepTime =
         tDurationAppBarAnimation.inMilliseconds / tStepsAppBarAnimation;
@@ -63,7 +55,6 @@ class AppBarCubit extends Cubit<AppBarState> {
     }
   }
 
-  /// Resets the app bar to its expanded state.
   void resetAppBar() {
     emit(const AppBarExpanded());
   }
--- lib/business_logic/blocs/appbar/app_bar_state.dart
+++ lib/business_logic/blocs/appbar/app_bar_state.dart
@@ -1,36 +1,26 @@
 part of 'app_bar_cubit.dart';
 
-/// The base state class for AppBar related states.
 abstract class AppBarState extends Equatable {
-  /// Constructor for [AppBarState].
   const AppBarState();
 }
 
-/// The state indicating that the AppBar is fully expanded.
 class AppBarExpanded extends AppBarState {
-  /// Constructor for [AppBarExpanded] state.
   const AppBarExpanded() : super();
 
   @override
   List<Object> get props => [];
 }
 
-/// The state indicating that the AppBar is collapsed.
 class AppBarCollapsed extends AppBarState {
-  /// Constructor for [AppBarCollapsed] state.
   const AppBarCollapsed() : super();
 
   @override
   List<Object> get props => [];
 }
 
-/// The state indicating that the AppBar is showing all filters
-/// with a specific height.
 class AppBarWithAllFilters extends AppBarState {
-  /// Constructor for [AppBarWithAllFilters] state with the given height.
   const AppBarWithAllFilters(this.heightAppBarWithFilters) : super();
 
-  /// The height of the AppBar when all filters are shown.
   final double heightAppBarWithFilters;
 
   @override
