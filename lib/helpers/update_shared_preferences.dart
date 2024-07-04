7f897a480a5ae3e770765f14e6418fc038f497f1
Umut Aktaş <aktasumut@gmail.com>
2024-07-04 21:06:47 +0200

    update, apply linter rules

--- lib/helpers/update_shared_preferences.dart
+++ lib/helpers/update_shared_preferences.dart
@@ -1,6 +1,9 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-Future<void> updateSharedPreferences(String key, bool isLiked) async {
+Future<void> updateSharedPreferences(
+  String key, {
+  required bool isLiked,
+}) async {
   final prefs = await SharedPreferences.getInstance();
   await prefs.setBool(key, isLiked);
 }
